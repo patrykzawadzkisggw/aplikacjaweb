da958e24badfade7cd6b07361b6c21e3498629e5
patrykzawadzkisggw <115794809+patrykzawadzkisggw@users.noreply.github.com>
2025-02-17 16:46:23 +0100

    Add support for RedHat

--- app/connect.php
+++ app/connect.php
@@ -4,7 +4,7 @@ $user = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 $db_name = getenv('MYSQL_DATABASE');
 try {
-    $db = new PDO("mysql:host=$host;dbname=$db_name",$user,$password);
+    $db = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8",$user,$password);
 } catch(Exception $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
